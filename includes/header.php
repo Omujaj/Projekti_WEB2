283f81a1fa67b9cad08eb320f3070bd8731cc538
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-05-26 14:04:08 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,14 @@
 <?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+
+if (!isset($pageTitle)) $pageTitle = 'Library';
 ?>
 <!DOCTYPE html>
-<html lang="sq">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Library - Library Management System</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="./assets/css/auth.css">
-     <link rel="stylesheet" href=./assets/css/catalog.css>
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Playfair+Display:wght@600;700;900&display=swap" rel="stylesheet">
+    <title><?= sanitize($pageTitle) ?> — Library</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=DM+Sans:wght@400;500;600&display=swap" rel="stylesheet">
 </head>
 <body>
