9a6251db5f2ced0d0d8eb9889c3905138afd0548
leonadervishajj <leona.dervishaj@student.uni-pr.edu>
2026-05-26 14:04:06 +0200

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,8 @@
 <footer class="footer">
-    <p>&copy; 2026 Library — Library Management System</p>
-</footer>
-
-<script src="assets/js/main.js"></script>
+        <div class="footer-inner">
+         <p>© <?= date('Y') ?> Library —  Library Management System</p>
+        </div>
+    </footer>
+    <script src="../assets/js/main.js"></script>
 </body>
 </html>
